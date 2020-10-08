b3bb0fca5b29b9a33931896a5524dc2f110ae1b2
Ramona <ramona.maria.gihr@hs-furtwangen.de>
2020-10-08 17:54:40 +0200

    changed stuff

--- Extraaufgabe_01/script.js
+++ Extraaufgabe_01/script.js
@@ -1,8 +1,6 @@
 "use strict";
 window.onload = function () {
     let person = prompt("What's your name?", "Enter here");
-    if (person != null) {
-        document.getElementById("hello").innerHTML = "Hello " + person + "!";
-    }
+    document.getElementById("hello").innerHTML = "Hello " + person + "!";
 };
 //# sourceMappingURL=script.js.map
--- Extraaufgabe_01/script.ts
+++ Extraaufgabe_01/script.ts
@@ -1,6 +1,4 @@
 window.onload = function() {
     let person = prompt("What's your name?", "Enter here");
-    if (person != null) {
-      document.getElementById("hello").innerHTML = "Hello " + person + "!";
-    }
+    document.getElementById("hello").innerHTML = "Hello " + person + "!";
 }
