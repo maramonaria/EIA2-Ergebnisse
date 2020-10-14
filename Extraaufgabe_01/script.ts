28db19929f56b4d18bf1b2fda67709b21be6838c
Ramona <ramona.maria.gihr@hs-furtwangen.de>
2020-10-14 23:32:56 +0200

    Aufgabe L01

--- Extraaufgabe_01/script.ts
+++ Extraaufgabe_01/script.ts
@@ -1,4 +1,4 @@
 window.onload = function() {
     let person = prompt("What's your name?", "Enter here");
-    document.getElementById("hello").innerHTML = "Hello " + person + "!";
+    document.getElementById("hello")!.innerHTML = "Hello " + person + "!";
 }
